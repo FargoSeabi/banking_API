f18a2bb7c176189b86dba4b85de052890564c829
MOSES SEABI <160843085+FargoSeabi@users.noreply.github.com>
2026-02-17 09:14:51 +0200

    Remove account registration route
    Removed account registration route from bankRoutes.js.

--- routes/bankRoutes.js
+++ routes/bankRoutes.js
@@ -7,27 +7,6 @@ const router = express.Router();
 /* =========================
    CREATE ACCOUNT
 ========================= */
-router.post("/register", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword
-    });
-
-    res.status(201).json({
-      message: "Account created successfully",
-      user
-    });
-
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
 
 /* =========================
    DEPOSIT
